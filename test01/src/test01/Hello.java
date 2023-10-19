c8fecaa49285ec9e4e5ef6928d48b8ebfc37f2bf
javagangyong <kenjisun@naver.com>
2023-10-19 14:48:41 +0900

    day01 OT

---
+++ test01/src/test01/Hello.java
@@ -0,0 +1,12 @@
+package test01;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, world !!");
+		// 저장 : ctrl + s
+		// 실행 : ctrl + F11
+		// syso 치고 ctrl + space = System.out.println
+		// 창 최대화/복구 : ctrl + m
+	}
+
+}
