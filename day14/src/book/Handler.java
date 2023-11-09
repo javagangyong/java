7639ee8681e38b98f8136f87121e177e77423fc0
javagangyong <kenjisun@naver.com>
2023-11-09 15:31:37 +0900

    상속, 추상클래스

--- day14/src/book/Handler.java
+++ day14/src/book/Handler.java
@@ -5,14 +5,14 @@ package book;
 
 public class Handler {
 	private Book[] arr = new Book[10];
-	
+
 	public Handler() {
 	}
-	
+
 	public int add(String bookName, int bookNumber) {
 		int row = 0;
-		for(int i = 0; i < arr.length; i++) {
-			if(arr[i] == null) {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == null) {
 				arr[i] = new Book();
 				arr[i].setBookName(bookName);
 				arr[i].setBookNumber(bookNumber);
@@ -22,14 +22,31 @@ public class Handler {
 		}
 		return row;
 	}
-	
+
+	public void sortByName() {
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] != null) {
+				for (int j = i + 1; j < arr.length; j++) {
+					if (arr[j] != null) {
+						int result = arr[i].getBookName().compareTo(arr[j].getBookName());
+						if (result > 0) {
+							String tmp = arr[i].getBookName();
+							arr[i].setBookName(arr[j].getBookName());
+							arr[j].setBookName(tmp);
+						}
+					}
+				}
+			}
+		}
+
+	}
+
 	public void showList() {
-		for(int i = 0; i < arr.length; i++) {
-			if(arr[i] != null) {
-				System.out.printf("책이름 : %-10s /책넘버 : %d\n", arr[i].getBookName(), arr[i].getBookNumber());
+		for (int k = 0; k < arr.length; k++) {
+			if (arr[k] != null) {
+				System.out.printf("책이름 : %-10s /책넘버 : %d\n", arr[k].getBookName(), arr[k].getBookNumber());
 			}
 		}
 	}
-	
 
 }
--- day14/src/book/Main.java
+++ day14/src/book/Main.java
@@ -36,6 +36,7 @@ public class Main {
 				break;
 				
 			case 2 :
+				handler.sortByName();
 				handler.showList();
 				break;
 				
